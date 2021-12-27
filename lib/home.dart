abc3c2cbded5f112829db99370ceaa920e1bc238
TimothyJin26 <timothy.jin26@gmail.com>
2021-12-27 12:04:17 -0800

    App updates

--- lib/home.dart
+++ lib/home.dart
@@ -58,6 +58,9 @@ class TransitApp extends StatefulWidget {
 /// The widget is rendered based on the state defined here
 ///
 class _TransitAppState extends State<TransitApp> {
+
+  String MAP_STYLE = '[  {    "elementType": "geometry",    "stylers": [      {        "color": "#242f3e"      }    ]  },  {    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#746855"      }    ]  },  {    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#242f3e"      }    ]  },  {    "featureType": "administrative.locality",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#d59563"      }    ]  },  {    "featureType": "poi",    "stylers": [      {        "visibility": "off"      }    ]  },  {    "featureType": "poi",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#d59563"      }    ]  },  {    "featureType": "poi.park",    "stylers": [      {        "visibility": "on"      }    ]  },  {    "featureType": "poi.park",    "elementType": "geometry",    "stylers": [      {        "color": "#263c3f"      }    ]  },  {    "featureType": "poi.park",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#6b9a76"      }    ]  },  {    "featureType": "road",    "elementType": "geometry",    "stylers": [      {        "color": "#38414e"      }    ]  },  {    "featureType": "road",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#212a37"      },      {        "visibility": "simplified"      },      {        "weight": 2      }    ]  },  {    "featureType": "road",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#9ca5b3"      }    ]  },  {    "featureType": "road.highway",    "elementType": "geometry",    "stylers": [      {        "color": "#746855"      }    ]  },  {    "featureType": "road.highway",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#1f2835"      }    ]  },  {    "featureType": "road.highway",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#f3d19c"      }    ]  },  {    "featureType": "transit",    "stylers": [      {        "visibility": "off"      }    ]  },  {    "featureType": "transit",    "elementType": "geometry",    "stylers": [      {        "color": "#2f3948"      }    ]  },  {    "featureType": "transit.station",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#d59563"      }    ]  },  {    "featureType": "water",    "elementType": "geometry",    "stylers": [      {        "color": "#17263c"      }    ]  },  {    "featureType": "water",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#515c6d"      }    ]  },  {    "featureType": "water",    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#17263c"      }    ]  }]';
+
   bool darkModeOn = false;
 
   //SunsetHelper.isDark();
@@ -293,7 +296,7 @@ class _TransitAppState extends State<TransitApp> {
 
     WidgetsFlutterBinding.ensureInitialized();
     // Initialize without device test ids.
-    Admob.initialize();
+    //Admob.initialize();
     //Admob.initialize(testDeviceIds: ['F1D272CE815B4D550E263F55298A8A41', 'abed5c3de5e25479dc8213ade8ac8191']);
 
     // Wait for the stops to load before checking connectivity and loading buses
@@ -349,8 +352,14 @@ class _TransitAppState extends State<TransitApp> {
 
                 subscription?.resume();
                 positionStream?.resume();
-
                 initWithLocation();
+
+                // Force map to rerender to prevent white screen
+                if (darkModeOn) {
+                  mapController.setMapStyle(MAP_STYLE);
+                } else {
+                  mapController.setMapStyle("[]");
+                }
               }
             }),
         suspendingCallBack: () async => setState(() {
@@ -801,8 +810,7 @@ class _TransitAppState extends State<TransitApp> {
   Future<void> _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
     if (darkModeOn) {
-      mapController.setMapStyle(
-          '[  {    "elementType": "geometry",    "stylers": [      {        "color": "#242f3e"      }    ]  },  {    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#746855"      }    ]  },  {    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#242f3e"      }    ]  },  {    "featureType": "administrative.locality",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#d59563"      }    ]  },  {    "featureType": "poi",    "stylers": [      {        "visibility": "off"      }    ]  },  {    "featureType": "poi",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#d59563"      }    ]  },  {    "featureType": "poi.park",    "stylers": [      {        "visibility": "on"      }    ]  },  {    "featureType": "poi.park",    "elementType": "geometry",    "stylers": [      {        "color": "#263c3f"      }    ]  },  {    "featureType": "poi.park",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#6b9a76"      }    ]  },  {    "featureType": "road",    "elementType": "geometry",    "stylers": [      {        "color": "#38414e"      }    ]  },  {    "featureType": "road",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#212a37"      },      {        "visibility": "simplified"      },      {        "weight": 2      }    ]  },  {    "featureType": "road",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#9ca5b3"      }    ]  },  {    "featureType": "road.highway",    "elementType": "geometry",    "stylers": [      {        "color": "#746855"      }    ]  },  {    "featureType": "road.highway",    "elementType": "geometry.stroke",    "stylers": [      {        "color": "#1f2835"      }    ]  },  {    "featureType": "road.highway",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#f3d19c"      }    ]  },  {    "featureType": "transit",    "stylers": [      {        "visibility": "off"      }    ]  },  {    "featureType": "transit",    "elementType": "geometry",    "stylers": [      {        "color": "#2f3948"      }    ]  },  {    "featureType": "transit.station",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#d59563"      }    ]  },  {    "featureType": "water",    "elementType": "geometry",    "stylers": [      {        "color": "#17263c"      }    ]  },  {    "featureType": "water",    "elementType": "labels.text.fill",    "stylers": [      {        "color": "#515c6d"      }    ]  },  {    "featureType": "water",    "elementType": "labels.text.stroke",    "stylers": [      {        "color": "#17263c"      }    ]  }]');
+      mapController.setMapStyle(MAP_STYLE);
     } else {
       mapController.setMapStyle("[]");
     }
@@ -861,7 +869,8 @@ class _TransitAppState extends State<TransitApp> {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-          home: Scaffold(
+    debugShowCheckedModeBanner: false,
+      home: Scaffold(
         key: _scaffoldKey,
         body: Stack(children: <Widget>[
           GoogleMap(
@@ -1378,7 +1387,7 @@ class _TransitAppState extends State<TransitApp> {
               ),
             ),
           ),
-          Positioned(
+          /*Positioned(
             bottom: 0,
             left: 0,
             right: 0,
@@ -1423,7 +1432,7 @@ class _TransitAppState extends State<TransitApp> {
                     }
                   }),
             ),
-          ),
+          ),*/
           Visibility(
             visible: shouldShowTranslinkOutage && isSelected[0],
             // Display only on the "buses" tab
